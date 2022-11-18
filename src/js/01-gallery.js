1f3b2dc288cedbc3caa64d4254d21cfdf5e4b9f7
Tanya Kyrylenko <nanalenko1905@gmail.com>
2022-11-18 14:58:20 +0200

    add hw

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,22 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+
+import { galleryItems } from './gallery-items.js';
+// Описан в документации
+import SimpleLightbox from "simplelightbox";
+// Дополнительный импорт стилей
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+const gallery = document.querySelector('.gallery')
+function createGallery({preview, original, description}){
+    return `
+    <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+}
+const createGalleryList = galleryItems.map(createGallery).join('');
+
+gallery.insertAdjacentHTML('afterbegin', createGalleryList);
+const lightbox = new SimpleLightbox('.gallery a', { captionDelay: 250, captionsData: "alt" });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+const refForm = document.querySelector('.feedback-form');
+
+restoreUserData();
+
+refForm.addEventListener('submit', onFormSubmit);
+refForm.addEventListener('input', throttle(onFormChange, 500));
+
+
+function onFormChange(e) {
+  let userData = localStorage.getItem(STORAGE_KEY);
+
+  userData = userData ? JSON.parse(userData) : {};
+  userData[e.target.name] = e.target.value;
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(userData));
+}
+
+function restoreUserData() {
+  let savedUserData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+  if (savedUserData) {
+    Object.entries(savedUserData).forEach(([name, value]) => {
+      refForm.elements[name].value = value;
+    });
+  }
+}
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  e.currentTarget.reset();
+  console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+  localStorage.removeItem(STORAGE_KEY);
+}
